b982ccffc4d3e9a38d739d96a39e26f5a7a32bf3
Kevga <9109613+Kevga@users.noreply.github.com>
2023-05-31 18:15:20 +0200

    Resize speech bubbles

---
+++ PriconneTLFixup/Patches/SpeechBubblePatch.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using BepInEx.Unity.IL2CPP.Utils.Collections;
+using Elements;
+using HarmonyLib;
+
+namespace PriconneTLFixup.Patches;
+
+[HarmonyPatch(typeof(PartsRoomBalloon), nameof(PartsRoomBalloon.ShowSpeakingText))]
+[HarmonyWrapSafe]
+public class SpeechBubblePatch
+{
+    public static void Postfix(PartsRoomBalloon __instance, string setText, RoomCharacter character)
+    {
+        if (setText.IsNullOrEmpty())
+        {
+            return;
+        }
+        
+        character.StartCoroutine(UpdateSpeechBubble(__instance, setText).WrapToIl2Cpp());
+    }
+
+    private static IEnumerator UpdateSpeechBubble(PartsRoomBalloon balloon, string originalText)
+    {
+        var text = originalText;
+        while (balloon.SpeakingActive)
+        {
+            if (balloon.speakingText.text != text)
+            {
+                text = balloon.speakingText.text;
+                balloon.speakingBalloon.SetDimensions(balloon.speakingBalloon.width, balloon.speakingText.height + 30);
+            }
+
+            yield return null;
+        }
+    }
+}
