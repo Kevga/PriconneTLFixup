9c6897570a5848da5b4220c64897b7e465d4b186
Kevga <9109613+Kevga@users.noreply.github.com>
2023-06-02 02:23:02 +0200

    Add HarmonyPatchController. Allows other patches to still run should one throw an exception.

---
+++ PriconneTLFixup/HarmonyPatchController.cs
@@ -0,0 +1,55 @@
+﻿using System.Reflection;
+using HarmonyLib;
+
+namespace PriconneTLFixup;
+
+public class HarmonyPatchController
+{
+    private List<PatchClassProcessor>? _patchClassProcessorList;
+    private Harmony _harmonyInstance = null!;
+    private const string HarmonyID = "com.github.kevga.priconnetlfixup";
+
+    private void InitPatches()
+    {
+        _harmonyInstance = new Harmony(HarmonyID);
+
+        _patchClassProcessorList = new List<PatchClassProcessor>();
+        AccessTools.GetTypesFromAssembly(Assembly.GetExecutingAssembly()).Do<Type>(type =>
+            {
+                if (type.FullName?.StartsWith("PriconneTLFixup.Patches") ?? false)
+                {
+                    try
+                    {
+                        _patchClassProcessorList.Add(_harmonyInstance.CreateClassProcessor(type));  
+                    } catch (Exception e)
+                    {
+                        Plugin.Logger.LogError(e);
+                    }
+                    
+                }
+            }
+        );
+    }
+
+    internal void PatchAll()
+    {
+        InitPatches();
+
+        _patchClassProcessorList?.ForEach(patchClassProcessor =>
+        {
+            try
+            {
+                patchClassProcessor.Patch();
+            }
+            catch (Exception e)
+            {
+                Plugin.Logger.LogError(e);
+            }
+        });
+    }
+
+    internal void UnpatchSelf()
+    {
+        _harmonyInstance.UnpatchSelf();
+    }
+}
--- PriconneTLFixup/Plugin.cs
+++ PriconneTLFixup/Plugin.cs
@@ -1,7 +1,6 @@
 ﻿using BepInEx;
 using BepInEx.Logging;
 using BepInEx.Unity.IL2CPP;
-using HarmonyLib;
 
 namespace PriconneTLFixup;
 
@@ -10,18 +9,19 @@ namespace PriconneTLFixup;
 public class Plugin: BasePlugin
 {
     internal static ManualLogSource Logger = null!;
-    private readonly Harmony _harmony = new("PriconneTLFixup");
+    private readonly HarmonyPatchController _harmonyController = new();
     
     public override void Load()
     {
         Logger = Log;
-        _harmony.PatchAll();
+        _harmonyController.PatchAll();
         Logger.LogInfo("PriconneTLFixup loaded!");
     }
     
     public override bool Unload()
     {
-        _harmony.UnpatchSelf();
+        Logger.LogInfo("Shutting down...");
+        _harmonyController.UnpatchSelf();
         return true;
     }
 }
