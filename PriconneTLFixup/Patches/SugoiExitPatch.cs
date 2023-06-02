5a0d567cbbfc8c4e77add72ebddc78166b40fda0
Kevga <9109613+Kevga@users.noreply.github.com>
2023-06-02 02:28:34 +0200

    Kill Sugoi process after quitting the game

---
+++ PriconneTLFixup/Patches/SugoiExitPatch.cs
@@ -0,0 +1,28 @@
+﻿using System.Diagnostics;
+using Cute;
+using HarmonyLib;
+using SugoiOfflineTranslator;
+
+namespace PriconneTLFixup.Patches;
+
+[HarmonyPatch(typeof(SugoiOfflineTranslatorEndpoint), "StartProcess")]
+public class SugoiExitPatch
+{
+    internal static Process? process;
+    
+    public static void Postfix(SugoiOfflineTranslatorEndpoint __instance, Process ___process)
+    {
+        Plugin.Logger.LogInfo("SugoiOfflineTranslatorEndpoint.StartProcess");
+        process = ___process;
+    }
+}
+
+[HarmonyPatch(typeof(Toolbox), nameof(Toolbox.ApplicationQuit))]
+public class SugoiExitPatch2
+{
+    public static void Prefix()
+    {
+        Plugin.Logger.LogInfo("Toolbox.ApplicationQuit");
+        SugoiExitPatch.process?.Kill();
+    }
+}
