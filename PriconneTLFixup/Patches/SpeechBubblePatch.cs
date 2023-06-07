4e298accdafa76736ab6ec9202414cf6c74055bb
Kevga <9109613+Kevga@users.noreply.github.com>
2023-06-07 21:10:06 +0200

    Add delay to speech bubbles that otherwise disappear immediately

--- PriconneTLFixup/Patches/SpeechBubblePatch.cs
+++ PriconneTLFixup/Patches/SpeechBubblePatch.cs
@@ -6,6 +6,10 @@ using UnityEngine;
 
 namespace PriconneTLFixup.Patches;
 
+/**
+ * Enables word-wrapping on speech bubbles, as well as disabling the typewriter effect on them.
+ * To fix an issue with bubbles that disappear shortly after the typewriter effect has finished, a delay is added.
+ */
 [HarmonyPatch(typeof(PartsRoomBalloon), nameof(PartsRoomBalloon.ShowSpeakingText))]
 [HarmonyWrapSafe]
 public class SpeechBubblePatch
@@ -47,7 +51,7 @@ public class SpineDramaControllerBalloonPatch
             return false;
         }
         
-        int key = __instance.intParse(__instance.nextParam());
+        var key = __instance.intParse(__instance.nextParam());
         
         var balloon = __instance.textBalloonDic[key];
         balloon.balloonSprite.width = 500;
@@ -55,44 +59,75 @@ public class SpineDramaControllerBalloonPatch
         balloon.textLabel.overflowMethod = UILabel.Overflow.ResizeHeight;
         balloon.textLabel.spacingY = 6;
         
-        string text = __instance.nextParam();
-        int charsPerSecond = __instance.intParse(__instance.nextParam());
-        bool tapSkip = __instance.boolParse(__instance.nextParam());
-        bool tapWait = __instance.boolParse(__instance.nextParam());
-        float offsetX = __instance.floatParse(__instance.nextParam());
-        float offsetY = __instance.floatParse(__instance.nextParam());
-        float fadeoutDelayOffset = __instance.floatParse(__instance.nextParam());
+        var text = __instance.nextParam();
+        var charsPerSecond = __instance.intParse(__instance.nextParam());
+        var tapSkip = __instance.boolParse(__instance.nextParam());
+        var tapWait = __instance.boolParse(__instance.nextParam());
+        var offsetX = __instance.floatParse(__instance.nextParam());
+        var offsetY = __instance.floatParse(__instance.nextParam());
+        var fadeoutDelayOffset = __instance.floatParse(__instance.nextParam());
         if (!__instance.textBalloonDic.ContainsKey(key))
         {
             return false;
         }
-        __instance.coroutineDic[__instance.CurrentCommandId] = __instance.coroutinePlayer.StartCoroutine(__instance.textBalloonDic[key].PlayCoroutine(text, charsPerSecond, tapSkip, tapWait, offsetX, offsetY, fadeoutDelayOffset));
-        __instance.coroutinePlayer.StartCoroutine(UpdateSpeechBubble(balloon, text).WrapToIl2Cpp());
+        
+        var correctedDelay = fadeoutDelayOffset;
+        if (text.Length > 0 && !tapSkip)
+        {
+            if (charsPerSecond > 0)
+            {
+                correctedDelay += text.Length / (float)charsPerSecond;
+            }
+            else
+            {
+                correctedDelay += 5;
+                Plugin.Logger.LogWarning("charsPerSecond was 0, using default delay of 5 seconds");
+            }
+            
+            Plugin.Logger.LogDebug($"Corrected delay: {correctedDelay} from {fadeoutDelayOffset} + {text.Length} / {charsPerSecond}");
+        }
+        else
+        {
+            Plugin.Logger.LogDebug($"Not correcting delay, text length: {text.Length}, tapSkip: {tapSkip}");
+        }
+        
+        //Check if the label text setter was intercepted by XUAT
+        balloon.textLabel.SetText(text);
+        // ReSharper disable once RedundantCheckBeforeAssignment
+        if (balloon.textLabel.text != text)
+        {
+            text = balloon.textLabel.text;
+        }
+        __instance.coroutineDic[__instance.CurrentCommandId] = __instance.coroutinePlayer.StartCoroutine(__instance.textBalloonDic[key].PlayCoroutine(text, charsPerSecond, tapSkip, tapWait, offsetX, offsetY, correctedDelay));
+        __instance.coroutinePlayer.StartCoroutine(UpdateSpeechBubble(balloon).WrapToIl2Cpp());
         return false;
     }
     
-    private static IEnumerator UpdateSpeechBubble(SpineDramaTextBalloon balloon, string originalText)
+    private static IEnumerator UpdateSpeechBubble(SpineDramaTextBalloon balloon)
     {
-        var text = originalText;
+        var text = "";
+        
         while (balloon.isActiveAndEnabled)
         {
-            if (balloon.textLabel.text != text)
+            var newText = balloon.textLabel.text.Trim();
+            if (text == "" && text != newText)
             {
-                text = balloon.textLabel.text;
-
-                if (text.Length < 2)
-                {
-                    yield return null;
-                }
+                text = newText;
+                continue;
+            }
+            
+            if (newText != text)
+            {
+                text = newText;
 
                 balloon.textLabel.ProcessText();
-                Vector2 printedSize = balloon.textLabel.printedSize;
+                var printedSize = balloon.textLabel.printedSize;
                 printedSize.x += balloon.MARGIN + balloon.MARGIN;
                 printedSize.y += balloon.MARGIN + balloon.MARGIN;
                 printedSize.x = Mathf.Max(printedSize.x, balloon.MIN_SIZE.x);
                 printedSize.y = Mathf.Max(printedSize.y, balloon.MIN_SIZE.y);
                 balloon.balloonSprite.SetDimensions((int)printedSize.x, (int)printedSize.y);
-                UIWidget.Pivot pivot = balloon.balloonSprite.pivot;
+                var pivot = balloon.balloonSprite.pivot;
                 if (pivot == UIWidget.Pivot.Left)
                 {
                     balloon.textLabel.transform.localPosition = new Vector3(balloon.MARGIN, printedSize.y / 2f - balloon.MARGIN);
