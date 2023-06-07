5af23fedacf6b9034c2c1d07b546397a7657ed67
Kevga <9109613+Kevga@users.noreply.github.com>
2023-06-07 21:03:02 +0200

    Remove unwanted characters that Sugoi produces

--- PriconneTLFixup/Patches/TranslationPreprocessorPatch.cs
+++ PriconneTLFixup/Patches/TranslationPreprocessorPatch.cs
@@ -15,10 +15,19 @@ public static class TranslationPreprocessorPatch
     private static readonly Regex ColorCodeRegex = new Regex(@"\[([0-9A-F]{8})\]", RegexOptions.Compiled);
     public static void Prefix(AutoTranslationPlugin __instance, object ui, ref string text, string originalText)
     {
+        if (originalText == null)
+        {
+            return;
+        }
+
         var colorCodeMatch = ColorCodeRegex.Matches(originalText);
         if (colorCodeMatch.Count > 0)
         {
             text = colorCodeMatch[^1].Value + text;
         }
+
+        text = text.Replace("⁇", "");
+        text = text.Replace("unk>", "");
+        text = text.Trim();
     }
 }
