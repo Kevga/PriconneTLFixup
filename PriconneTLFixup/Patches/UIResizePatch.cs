0bc08a61fb88b6dcff3321dc8d4ff1c03347b572
Kevga <9109613+Kevga@users.noreply.github.com>
2023-06-07 21:12:08 +0200

    Reduce font size of boss HP if it overlaps with a mode indicator

--- PriconneTLFixup/Patches/UIResizePatch.cs
+++ PriconneTLFixup/Patches/UIResizePatch.cs
@@ -7,6 +7,7 @@ namespace PriconneTLFixup.Patches;
  * The Grotto button text is much smaller than the surrounding ones. Scale it up to match.
  */
 [HarmonyPatch(typeof(ViewQuestTop), nameof(ViewQuestTop.StartView))]
+[HarmonyWrapSafe]
 public class GrottoButtonPatch
 {
     public static void Postfix(ViewQuestTop __instance)
@@ -19,11 +20,12 @@ public class GrottoButtonPatch
  * Same with the settings button.
  */
 [HarmonyPatch(typeof(ViewMenuTop), nameof(ViewMenuTop.StartView))]
+[HarmonyWrapSafe]
 public class SettingsButtonPatch
 {
     public static void Postfix(ViewMenuTop __instance)
     {
-        __instance.systemButton.GetChildUILabel().lineWidth = 95;
+        __instance.systemButton.GetChildUILabel().lineWidth = 115;
         __instance.cartoonButton.GetChildUILabel().lineWidth = 190;
     }
 }
@@ -33,6 +35,7 @@ public class SettingsButtonPatch
  * For the latin alphabet, character width is smaller, so we scale down the underline length.
  */
 [HarmonyPatch(typeof(PartsHeaderBackButton), nameof(PartsHeaderBackButton.SetTitleText))]
+[HarmonyWrapSafe]
 public class HeaderUnderlinePatch
 {
     public static void Postfix(PartsHeaderBackButton __instance)
@@ -41,5 +44,21 @@ public class HeaderUnderlinePatch
         __instance.underLine.width = newUnderlineWidth;
         var headerController = SingletonMonoBehaviour<HeaderController>.Instance;
         headerController.campaignIcons.SetIconPosition(headerController.viewManager.CurrentViewId, newUnderlineWidth - 20);
+    }
+}
+
+/**
+ * "Special" battles have a mode indicator that the HP can overlap with, so we scale the HP text down.
+ */
+[HarmonyPatch(typeof(PartsBossGauge), nameof(PartsBossGauge.InitGauge))]
+[HarmonyWrapSafe]
+public class BossGaugePatch
+{
+    public static void Postfix(PartsBossGauge __instance)
+    {
+        if (__instance.battleManager.IsSpecialBattle)
+        {
+            __instance.hpLabel.fontSize = 16;
+        }
     }
 }
