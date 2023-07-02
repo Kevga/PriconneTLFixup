a1237411b0d2a208d08d44264c609f6fc8457c17
Kevga <9109613+Kevga@users.noreply.github.com>
2023-07-02 18:22:47 +0200

    Color code fixes

--- PriconneTLFixup/Patches/TranslationPreprocessorPatch.cs
+++ PriconneTLFixup/Patches/TranslationPreprocessorPatch.cs
@@ -1,18 +1,24 @@
 ﻿using System.Text.RegularExpressions;
 using HarmonyLib;
 using XUnity.AutoTranslator.Plugin.Core;
+using Fastenshtein;
+
 // ReSharper disable InconsistentNaming
 
 namespace PriconneTLFixup.Patches;
 
 /**
- * Color code fix. They may be getting removed by automated translation, this patch adds them back.
+ * Color code fixes. They frequently get mangled by the MTL. We try to restore them.
  */
 [HarmonyPatch(typeof(AutoTranslationPlugin), "SetText")]
 [HarmonyWrapSafe]
 public static class TranslationPreprocessorPatch
 {
-    private static readonly Regex ColorCodeRegex = new Regex(@"\[([0-9A-F]{8})\]", RegexOptions.Compiled);
+    private static readonly Regex ColorCodeRegex = new(@"\[([0-9A-Fa-fS]{6,10})\]", RegexOptions.Compiled);
+    private static readonly Regex ColorGradientRegex = new(@"\[([0-9A-Fa-f,S\s]{13,20})\]", RegexOptions.Compiled);
+    private const int COLOR_DISTANCE_THRESHOLD = 3;
+    private const int GRADIENT_DISTANCE_THRESHOLD = 5;
+
     public static void Prefix(AutoTranslationPlugin __instance, object ui, ref string text, string originalText)
     {
         if (originalText == null)
@@ -20,12 +26,60 @@ public static class TranslationPreprocessorPatch
             return;
         }
 
-        var colorCodeMatch = ColorCodeRegex.Matches(originalText);
-        if (colorCodeMatch.Count > 0)
+        var preTLColorMatches = ColorCodeRegex.Matches(originalText);
+        var distinctPreTLColorMatches = preTLColorMatches.OfType<Match>().GroupBy(x => x.Value).Select(x =>x.First()).ToList();
+        if (distinctPreTLColorMatches.Count > 0)
         {
-            text = colorCodeMatch[^1].Value + text;
+            text = preTLColorMatches[^1].Value + text;
+        }
+        
+        var postTLColorMatches = ColorCodeRegex.Matches(text);
+        
+        foreach (var preTLColorMatch in distinctPreTLColorMatches)
+        {
+            Levenshtein Levenshtein = new(preTLColorMatch.Value);
+            foreach (Match postTLColorMatch in postTLColorMatches)
+            {
+                if (postTLColorMatch.Value == preTLColorMatch.Value)
+                {
+                    continue;
+                }
+                
+                if (Levenshtein.DistanceFrom(postTLColorMatch.Value) <= COLOR_DISTANCE_THRESHOLD )
+                {
+                    Log.Debug("Replacing color due to Levenshtein match: " + postTLColorMatch.Value + " -> " + preTLColorMatch.Value);
+                    text = text.Replace(postTLColorMatch.Value, preTLColorMatch.Value);
+                }
+            }
         }
 
+        var colorGradientMatchesPre = ColorGradientRegex.Matches(originalText);
+        var colorGradientMatchsPost = ColorGradientRegex.Matches(text);
+        var distinctPreTLGradientMatches = colorGradientMatchesPre.OfType<Match>().GroupBy(x => x.Value).Select(x =>x.First()).ToList();
+        
+        foreach (var preTLGradientMatch in distinctPreTLGradientMatches)
+        {
+            Log.Debug("PreTLGradientMatch: " + preTLGradientMatch.Value);
+            Levenshtein Levenshtein = new(preTLGradientMatch.Value);
+            foreach (Match postTLGradientMatch in colorGradientMatchsPost)
+            {
+                Log.Debug("PostTLGradientMatch: " + postTLGradientMatch.Value);
+                
+                if (Levenshtein.DistanceFrom(postTLGradientMatch.Value) <= GRADIENT_DISTANCE_THRESHOLD)
+                {
+                    Log.Debug("Replacing gradient due to Levenshtein match: " + postTLGradientMatch.Value + " -> " + preTLGradientMatch.Value);
+                    text = text.Replace(postTLGradientMatch.Value, preTLGradientMatch.Value);
+                }
+                
+                if (preTLGradientMatch.Value == postTLGradientMatch.Value.Replace(" ", ""))
+                {
+                    Log.Debug("Removing spaces from gradient match: " + postTLGradientMatch.Value + " -> " + postTLGradientMatch.Value.Replace(" ", ""));
+                    text = text.Replace(postTLGradientMatch.Value, postTLGradientMatch.Value.Replace(" ", ""));
+                }
+            }
+        }
+
+        text = text.Replace("[--]", "[-]");
         text = text.Replace("⁇", "");
         text = text.Replace("unk>", "");
         text = text.Trim();
