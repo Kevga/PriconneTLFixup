845b6aaabd67f5beb5d7977c5b5f73342181f460
Kevga <9109613+Kevga@users.noreply.github.com>
2023-07-23 13:10:42 +0200

    Move auto progression sprite to center it

---
+++ PriconneTLFixup/Patches/UIMovePatch.cs
@@ -0,0 +1,32 @@
+﻿using Elements;
+using HarmonyLib;
+using UnityEngine;
+
+namespace PriconneTLFixup.Patches;
+
+[HarmonyPatch(typeof(BattleHeaderController), nameof(BattleHeaderController.Init))]
+public class UIMovePatch
+{
+    public static void Postfix(BattleHeaderController __instance)
+    {
+        if (__instance == null || __instance.battleManager == null)
+        {
+            return;
+        }
+
+        if (!__instance.battleManager.IsQuestAutoProgressing())
+        {
+            return;
+        }
+        
+        var go = GameObject.Find("_Game(Clone)/UI Root/ViewsArea/View/ViewBattle(Clone)/Header/AnchorTopLeft/AutoProgressInfoParent/PartsEventQuestAutoProgressInBattleInfo(Clone)/GUIGroup_QuestAuto/questauto_text_auro/");
+        if (go == null)
+        {
+            return;
+        }
+        
+        var pos = go.transform.localPosition;
+        pos.x = -25;
+        go.transform.localPosition = pos;
+    }
+}
