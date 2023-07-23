6b3dea05d37dc0a8750c3944dd6ce62cc2e63ba3
Kevga <9109613+Kevga@users.noreply.github.com>
2023-07-23 13:11:17 +0200

    Implement unit and shop search for english text

---
+++ PriconneTLFixup/Patches/ShopSearchPatch.cs
@@ -0,0 +1,82 @@
+﻿using Elements;
+using HarmonyLib;
+
+namespace PriconneTLFixup.Patches;
+
+[HarmonyPatch(typeof(PartsShopFooter), nameof(PartsShopFooter._MaterialFilterBySearchText_b__109_0))]
+[HarmonyWrapSafe]
+public class ShopSearchPatch
+{
+    public static bool Prefix(PartsShopFooter __instance, ShopItem item, ref bool __result)
+    {
+        if (item.Type != eInventoryType.Item)
+        {
+            __result = false;
+            return false;
+        }
+        
+        var source = __instance.masterDataManager.masterItemData.Get(item.ItemId)?.MaterialSearchText;
+        if (source == null)
+        {
+            __result = false;
+            return false;
+        }
+        
+        var filteredSource = source.Replace("のメモリーピース", "").Replace("のピュアメモリーピース", "");
+        if (filteredSource.IsNullOrEmpty())
+        {
+            __result = false;
+            return false;
+        }
+        
+        var found = DictPatch.NameDict.TryGetValue(filteredSource, out var enSpellings);
+        if (!found || enSpellings == null)
+        {
+            Log.Warn($"UnitSort.MatchSearchFilter: {nameof(source)} not found in dictionary: {filteredSource}");
+            __result = false;
+            return true;
+        }
+
+        var lowerCaseInput = __instance.LastMaterialSearchText.ToLower();
+        var match = false;
+        for (var i = 0; i < enSpellings.Length; i++)
+        {
+            var enSpelling = enSpellings[i];
+            if (enSpelling.ToLower().StartsWith(lowerCaseInput, true, null, UnitDefine.UnitNameSearchSplitString))
+            {
+                match = true;
+                break;
+            }
+        }
+       
+        __result = match;
+        return false;
+    }
+}
+
+//Only works for master shop. Has the side effect that the placeholder vanishes after switching tabs. Not ideal.
+/*[HarmonyPatch(typeof(PartsShopFooter), nameof(PartsShopFooter.SetTab))]
+[HarmonyWrapSafe]
+public class ShopSearchFocusPatch
+{
+    public static void Postfix(PartsShopFooter __instance)
+    {
+        if (!__instance.itemSearchGroupOrDefault.ItemNameInput.selected)
+        {
+            __instance.itemSearchGroupOrDefault.ItemNameInput.OnSelect(true);    
+        }
+    }
+}*/
+
+//This function filters out the parentheses from unit names.
+//However, our dictionary contains them, so we return the original string.
+[HarmonyPatch(typeof(ShopUtility), nameof(ShopUtility.GetMaterialSearchTextFormat))]
+[HarmonyWrapSafe]
+public class ShopSearchFormatPatch
+{
+    public static bool Prefix(string _source, ref string __result)
+    {
+        __result = _source;
+        return false;
+    }
+}
---
+++ PriconneTLFixup/Patches/UnitSearchPatch.cs
@@ -0,0 +1,90 @@
+using System.Collections;
+using BepInEx;
+using Cute;
+using Elements;
+using HarmonyLib;
+using PriconneTLFixup;
+
+namespace PriconneTLFixup.Patches;
+
+[HarmonyPatch(typeof(UnitSort), nameof(UnitSort.MatchSearchFilter))]
+[HarmonyWrapSafe]
+public class DictPatch
+{
+    private static readonly string DictPath = Path.Join(Paths.BepInExRootPath, "Translation", "en", "Other", "unit_names.txt");
+
+    public static void Prepare()
+    {
+        ReadUnitNameFile();
+        Log.Info("Loaded names for " + NameDict.Count + " characters.");
+    }
+    
+    public static bool Prefix(ref bool __result, string _source, string _filter)
+    {
+        var found = NameDict.TryGetValue(_source, out var enSpellings);
+        if (!found || enSpellings == null)
+        {
+            Log.Warn($"UnitSort.MatchSearchFilter: {nameof(_source)} not found in dictionary: {_source}");
+            __result = false;
+            return true;
+        }
+
+        var lowerCaseInput = _filter.ToLower();
+        var match = false;
+        for (var i = 0; i < enSpellings.Length; i++)
+        {
+            var enSpelling = enSpellings[i];
+            if (enSpelling.ToLower().StartsWith(lowerCaseInput, true, UnitDefine.UNIT_SEARCH_REMOVE_STRING, UnitDefine.UnitNameSearchSplitString))
+            {
+                match = true;
+                break;
+            }
+        }
+       
+        __result = match;
+        return false;
+    }
+    
+    internal static readonly Dictionary<string, string[]> NameDict = new();
+
+    private static void ReadUnitNameFile()
+    {
+        if (!File.Exists(DictPath))
+        {
+            Log.Error($"Unit name dictionary file not found: {DictPath}");
+            return;
+        }
+        
+        var file = new StreamReader(DictPath);
+        while (file.ReadLine() is { } ln)
+        {
+            var parts = ln.Split("=");
+            if (parts.Length != 2)
+            {
+                continue;
+            }
+
+            var jp = parts[0];
+            var en = parts[1];
+            if (en.Length == 0)
+            {
+                continue;
+            }
+
+            var enVariants = en.Split(";");
+
+            NameDict[jp] = enVariants.Where(enVariant => enVariant.Length > 0).ToArray();
+        }
+
+        file.Close();
+    }
+}
+
+[HarmonyPatch(typeof(SearchUnitNamePlate), nameof(SearchUnitNamePlate.Initialize))]
+public class AutoFocusSearchPatch
+{
+    public static void Postfix(SearchUnitNamePlate __instance)
+    {
+        __instance.input.OnSelect(true);
+    }
+}
