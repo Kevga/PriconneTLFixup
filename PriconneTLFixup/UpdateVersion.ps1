68b557a15fd5c6870db4273246f9fefef155d88b
Kevga <9109613+Kevga@users.noreply.github.com>
2023-07-23 12:58:41 +0200

    Automatically set BepInEx plugin version from csproj

---
+++ PriconneTLFixup/UpdateVersion.ps1
@@ -0,0 +1,18 @@
+$csprojPath = ".\PriconneTLFixup.csproj"
+$pluginClassPath = ".\Plugin.cs"
+$pluginClassRegex = '\[BepInPlugin\((?:\s*"(?<id>.+?)")\s*,\s*"(?<name>.+?)"\s*,\s*"(?<version>.+?)"\)\]'
+
+# Get the version from the csproj file
+$xml = [xml](Get-Content $csprojPath)
+$version = $xml.Project.PropertyGroup.Version
+
+# Update the plugin class with the new version
+$pluginClassContent = Get-Content $pluginClassPath -Raw
+$updatedPluginClassContent = [regex]::Replace($pluginClassContent, $pluginClassRegex, {
+    param($match)
+    $id = $match.Groups['id'].Value
+    $name = $match.Groups['name'].Value
+    "[BepInPlugin(`"$id`", `"$name`", `"$version`")]"  # Note the use of backticks to escape double quotes
+})
+# Save the updated content back to the plugin class file
+$updatedPluginClassContent | Set-Content $pluginClassPath
