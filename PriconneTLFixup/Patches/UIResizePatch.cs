99c5dace81328fdc98449c73c45a13a9d94c4f06
Kevga <9109613+Kevga@users.noreply.github.com>
2023-08-06 01:21:11 +0200

    Replace PartsHeaderBackButton.SetTitleText to prevent double assignment which causes problems with XUAT

--- PriconneTLFixup/Patches/UIResizePatch.cs
+++ PriconneTLFixup/Patches/UIResizePatch.cs
@@ -1,8 +1,10 @@
 ﻿using System.Collections;
 using System.Reflection;
+using System.Text.RegularExpressions;
 using BepInEx.Unity.IL2CPP.Utils.Collections;
 using Elements;
 using HarmonyLib;
+using UnityEngine;
 
 namespace PriconneTLFixup.Patches;
 
@@ -42,15 +44,31 @@ public class SettingsButtonPatch
 [HarmonyWrapSafe]
 public class TitleTextPatch
 {
-    public static void Postfix(PartsHeaderBackButton __instance, string _setTitleText)
+    public static bool Prefix(PartsHeaderBackButton __instance, string _setTitleText)
     {
-        if (_setTitleText == null || _setTitleText.Length == 0)
+        if (_setTitleText.IsNullOrEmpty())
         {
-            return;
+            __instance.subTitleLabel.SetActiveWithCheck(false);
+            return false;
         }
-        
-        __instance.titleLabel.text = "";
-        __instance.titleLabel2nd.text = _setTitleText;
+        var activeSelf = __instance.gameObject.activeSelf;
+        __instance.gameObject.SetActive(false);
+        __instance.gameObject.SetActive(true);
+        __instance.titleLabel.SetText("");
+        __instance.titleLabel2nd.SetText(_setTitleText);
+        __instance.titleLabel.SetActive(false);
+        __instance.titleLabel2nd.SetActive(false);
+        __instance.titleLabel.SetActive(true);
+        __instance.titleLabel2nd.SetActive(true);
+        __instance.gameObject.SetActive(activeSelf);
+        var matchCollection = Regex.Matches(__instance.titleLabel2nd.text, "[a-zA-Z0-9]");
+        var num = __instance.titleLabel2nd.text.Length - matchCollection.Count;
+        var fontSize = __instance.titleLabel2nd.fontSize;
+        var num2 = Mathf.CeilToInt(fontSize * 0.75f);
+        var num3 = __instance.titleLabel.text.Length * __instance.titleLabel.fontSize + num * fontSize + matchCollection.Count * num2;
+        __instance.underLine.width = __instance.leftOffset + num3 + __instance.rightOffset;
+        __instance.underLine.gameObject.SetActive(true);
+        __instance.subTitleLabel.SetActiveWithCheck(false);
         
         Log.Debug($"TitleTextPatch: _setTitleText = {_setTitleText}, text = {__instance.titleLabel2nd.text}, fontSize = {__instance.titleLabel2nd.fontSize}");
 
@@ -59,22 +77,25 @@ public class TitleTextPatch
         pos.x = __instance.backButton == null ? 22 : 72;
         transform.localPosition = pos;
 
-         var newUnderlineWidth =
-             Convert.ToInt16(__instance.titleLabel2nd.text.Length * __instance.titleLabel2nd.fontSize) + 20;
+        var newUnderlineWidth =
+            Convert.ToInt16(__instance.titleLabel2nd.text.Length * __instance.titleLabel2nd.fontSize) + 20;
         __instance.underLine.width = newUnderlineWidth;
         var headerController = SingletonMonoBehaviour<HeaderController>.Instance;
         headerController.campaignIcons.SetIconPosition(
             headerController.viewManager.CurrentViewId,
             newUnderlineWidth 
         );
+        
         CoroutineStarter.Instance.StartCoroutine(
-            WaitForTranslationCoroutine(__instance.titleLabel2nd, __instance.underLine, __instance.titleLabel2nd.text).WrapToIl2Cpp());
+            WaitForTranslationCoroutine(__instance.titleLabel2nd, __instance.underLine, _setTitleText).WrapToIl2Cpp());
+
+        return false;
     }
 
     private static IEnumerator WaitForTranslationCoroutine(UILabel label, UIWidget underline, string originalText)
     {
         Log.Debug($"Before: text = {label.text}, fontSize = {label.fontSize}");
-        var yieldInstruction = new Util.WaitForSecondsOrPredicate(0.2f, () => originalText != label.text);
+        var yieldInstruction = new Util.WaitForSecondsOrPredicate(5.0f, () => originalText != label.text);
         while (yieldInstruction.keepWaiting)
         {
             yield return null;
