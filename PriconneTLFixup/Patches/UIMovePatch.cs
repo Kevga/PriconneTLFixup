172b72f282f6a1bb2a9f9e13a4f9e3fe42b56afa
Kevga <9109613+Kevga@users.noreply.github.com>
2023-11-16 00:02:30 +0100

    User profile UI adjustments

--- PriconneTLFixup/Patches/UIMovePatch.cs
+++ PriconneTLFixup/Patches/UIMovePatch.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 namespace PriconneTLFixup.Patches;
 
 [HarmonyPatch(typeof(PartsEventQuestAutoProgressInBattleInfo), nameof(PartsEventQuestAutoProgressInBattleInfo.Initialize))]
+[HarmonyWrapSafe]
 public class UIMovePatch
 {
     public static void Postfix(PartsEventQuestAutoProgressInBattleInfo __instance)
@@ -26,6 +27,49 @@ public class UIMovePatch
         
         var pos = go.transform.localPosition;
         pos.x = -25;
+        go.transform.localPosition = pos;
+    }
+}
+
+//PartsDialogUserProfile.InitializeParam
+[HarmonyPatch(typeof(PartsDialogUserProfile), nameof(PartsDialogUserProfile.InitializeParam))]
+[HarmonyWrapSafe]
+public class UIMovePatch2
+{
+    public static void Postfix(PartsDialogUserProfile __instance)
+    {
+        if (__instance == null)
+        {
+            return;
+        }
+
+        //Tower label
+        var go = GameObject.Find("PartsDialogUserProfile(Clone)/ProfileProgressGroup/ScrollView/PartsProfileProgress/GUIGroup_ContentsTower/GUIGroup_title_tower/Label");
+        if (go == null)
+        {
+            return;
+        }
+        
+        var pos = go.transform.localPosition;
+        pos.x = 142;
+        go.transform.localPosition = pos;
+        
+        //Unlocked label
+        go = GameObject.Find("ProfileProgressGroup/ScrollView/PartsProfileProgress/GUIGroup_ContentsRelease/GUIGroup_title_Release/Label");
+        if (go == null)
+        {
+            return;
+        }
+
+        var label = go.GetComponent<CustomUILabel>();
+        if (label == null)
+        {
+            return;
+        }
+
+        label.lineWidth = 120;
+        pos = go.transform.localPosition;
+        pos.x = 136;
         go.transform.localPosition = pos;
     }
 }
