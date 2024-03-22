96003aafa54696d81de203941604819b7b0ac170
Kevga <9109613+Kevga@users.noreply.github.com>
2024-03-22 20:07:05 +0100

    Fix XUAT reference not being stored if no translation backend is set up

--- PriconneTLFixup/Patches/XUATPatch.cs
+++ PriconneTLFixup/Patches/XUATPatch.cs
@@ -10,12 +10,14 @@ public class XUATInstancePatch
 {
     public static void Postfix(AutoTranslationPlugin __instance)
     {
-        if (__instance == null || __instance.TranslationManager == null || __instance.TranslationManager.CurrentEndpoint == null)
+        Plugin.AutoTranslatorPlugin = __instance;
+        
+        if (__instance.TranslationManager == null || __instance.TranslationManager.CurrentEndpoint == null)
         {
             Log.Info("No XUAT endpoint set");
             return;
         }
-        Plugin.AutoTranslatorPlugin = __instance;
+        
         var enpoint = Plugin.AutoTranslatorPlugin?.TranslationManager?.CurrentEndpoint;
         Log.Info($"Current endpoint: {enpoint?.Endpoint.Id}");
         Log.Info($"Delay: {enpoint?.TranslationDelay}");
