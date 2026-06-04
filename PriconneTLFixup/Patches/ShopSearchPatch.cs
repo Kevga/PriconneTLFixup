8a65bbef211d995002fa48563367ab92dc784414
Kevga <9109613+Kevga@users.noreply.github.com>
2026-06-04 18:34:05 +0200

    Add support for english memory piece search

--- PriconneTLFixup/Patches/ShopSearchPatch.cs
+++ PriconneTLFixup/Patches/ShopSearchPatch.cs
@@ -1,4 +1,5 @@
 ﻿using Elements;
+using Elements.Data;
 using HarmonyLib;
 
 namespace PriconneTLFixup.Patches;
@@ -54,19 +55,77 @@ public class ShopSearchPatch
     }
 }
 
-//Only works for master shop. Has the side effect that the placeholder vanishes after switching tabs. Not ideal.
-/*[HarmonyPatch(typeof(PartsShopFooter), nameof(PartsShopFooter.SetTab))]
+[HarmonyPatch(typeof(PartsDialogItemSelect), nameof(PartsDialogItemSelect.searchItemExec))]
 [HarmonyWrapSafe]
-public class ShopSearchFocusPatch
+public class ItemSelectSearchPatch
 {
-    public static void Postfix(PartsShopFooter __instance)
+    private static bool MatchesEnglishSearch(MasterItemData masterItemData, int itemId, string lowerCaseSearch)
     {
-        if (!__instance.itemSearchGroupOrDefault.ItemNameInput.selected)
+        var source = masterItemData.Get(itemId)?.MaterialSearchText;
+        if (source == null) return false;
+
+        var filteredSource = source.Replace("のメモリーピース", "").Replace("のピュアメモリーピース", "");
+        if (filteredSource.IsNullOrEmpty()) return false;
+
+        if (!DictPatch.NameDict.TryGetValue(filteredSource, out var enSpellings) || enSpellings == null)
         {
-            __instance.itemSearchGroupOrDefault.ItemNameInput.OnSelect(true);    
+            Log.Warn($"ItemSelectSearchPatch: source not found in dictionary: {filteredSource}");
+            return false;
+        }
+
+        for (var i = 0; i < enSpellings.Length; i++)
+        {
+            if (enSpellings[i].ToLower().StartsWith(lowerCaseSearch, true, null, UnitDefine.UnitNameSearchSplitString))
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public static bool Prefix(PartsDialogItemSelect __instance)
+    {
+        __instance.searchItemList.Clear();
+        var lowerCaseSearch = __instance.searchWord.ToLower();
+
+        switch (__instance.tabIndex)
+        {
+            case PartsDialogItemSelect.eTabIndex.MEMORY_PIECE:
+            {
+                __instance.sortMaterial();
+                MasterItemData masterItemData = __instance.masterDataMgr.masterItemData;
+                for (int j = 0; j < __instance.dispMaterialParamList.Count; j++)
+                {
+                    int itemId = __instance.dispMaterialParamList[j].ItemId;
+                    if (MatchesEnglishSearch(masterItemData, itemId, lowerCaseSearch))
+                    {
+                        __instance.searchItemList.Add(__instance.dispMaterialParamList[j]);
+                    }
+                }
+                __instance.dispMaterialParamList = __instance.searchItemList;
+                return false;
+            }
+            case PartsDialogItemSelect.eTabIndex.SUPER_MEMORY_PIECE:
+            {
+                __instance.sortSuperMaterial();
+                MasterItemData masterItemData = __instance.masterDataMgr.masterItemData;
+                for (int k = 0; k < __instance.dispSuperMaterialParamList.Count; k++)
+                {
+                    int itemId = __instance.dispSuperMaterialParamList[k].ItemId;
+                    if (MatchesEnglishSearch(masterItemData, itemId, lowerCaseSearch))
+                    {
+                        __instance.searchItemList.Add(__instance.dispSuperMaterialParamList[k]);
+                    }
+                }
+                __instance.dispSuperMaterialParamList = __instance.searchItemList;
+                return false;
+            }
+            default:
+                return true;
         }
     }
-}*/
+}
 
 //This function filters out the parentheses from unit names.
 //However, our dictionary contains them, so we return the original string.
