5e0bbcc487ac561bfac676965aa34bbb8fede0b4
Kevga <9109613+Kevga@users.noreply.github.com>
2026-06-04 19:22:54 +0200

    Fix and clean up monster detail patch

--- PriconneTLFixup/Patches/MonsterDetailPatch.cs
+++ PriconneTLFixup/Patches/MonsterDetailPatch.cs
@@ -20,6 +20,13 @@ public class SkillDescriptionPatch
 {
     public static void Prefix(ref Il2CppSystem.Collections.Generic.List<string> _monsterDetailTextList)
     {
+        if (!Util.IsTranslationEnabled())
+        {
+            return;
+        }
+
+        Log.Debug("Combining monster detail text list into one string");
+        _monsterDetailTextList.ToArray().ForEach(new Action<string>(s => Log.Debug("Original string: " + s)));
         var finalList = new Il2CppSystem.Collections.Generic.List<string>();
         var _string = _monsterDetailTextList.ToArray().Join(null, "\n");
         finalList.Add(_string);
@@ -37,7 +44,7 @@ public class MonsterDetailScrollContainerPatch
     }
 }
 
-[HarmonyPatch(typeof(PartsMonsterDetailTextPlate), nameof(PartsMonsterDetailTextPlate.Initialize))]
+[HarmonyPatch(typeof(PartsMonsterDetailTextPlate), nameof(PartsMonsterDetailTextPlate.SetText))]
 [HarmonyWrapSafe]
 public class MonsterDetailOverflowPatch
 {
@@ -56,35 +63,82 @@ public class MonsterDetailOverflowPatch
 
     private static IEnumerator UpdateDetailTextPlate(PartsMonsterDetailTextPlate textPlate)
     {
+        var gameObject = textPlate.gameObject;
+        var parent = gameObject.transform.parent;
+        Transform lowestChild = null;
+        for (int i = 0; i < parent.childCount; i++)
+        {
+            var child = parent.GetChild(i);
+            if (lowestChild == null && child != gameObject.transform || child.position.y < lowestChild?.position.y)
+            {
+                lowestChild = child;
+            }
+        }
+
+        if (lowestChild == null)
+        {
+            Log.Debug("Could not find lowest child of PartsMonsterDetailTextPlate");
+        }
+        else
+        {
+            Log.Debug("Found lowest child of PartsMonsterDetailTextPlate");
+        }
+
         var text = "";
-        var separators = new[] {"・", "·"};
+        //var separators = new[] {"・", "·"};
         while (textPlate.detailText != null)
         {
             if (textPlate.detailText.text != text)
             {
                 Log.Debug("Updating PartsMonsterDetailTextPlate text");
-                var updatedText = textPlate.detailText.text;
-                foreach (var separator in separators)
-                {
-                    updatedText = updatedText.Replace(separator, "\n\n" + separator);
-                    updatedText = updatedText.Replace("\n\n\n" + separator, "\n\n" + separator);
-                }
-                textPlate.detailText.text = updatedText;
-                text = updatedText;
+                Log.Debug(textPlate.detailText.text);
+                text = textPlate.detailText.text;
+
                 yield return null;
-                var scrollView = GameObject.Find(
-                    "_Game(Clone)/UI Root/FrontArea/IgnoreBlurLayer/DialogArea/DialogCommonMiddle(Clone)/Main/PartsParent/PartsDialogMonsterDetail(Clone)/GUIPanel_DialogCommonSmall/GUIGroup_DialogContents/ScrollContent/ScrollView/WrapContent/PartsMonsterDetailTextPlate(Clone)/");
+                if (lowestChild != null)
+                {
+                    var textHeight = textPlate.detailText.height;
+                    Log.Debug($"Lowest child y: {lowestChild.localPosition.y}, height: {textHeight}");
+                    gameObject.transform.localPosition = new Vector3(gameObject.transform.localPosition.x,
+                        lowestChild.localPosition.y - textHeight / 2, gameObject.transform.localPosition.z);
+                }
+
+                yield return null;
+                yield return null;
+                yield return null;
+
+                UIDragScrollView scrollRect = null!;
+                scrollRect = textPlate.transform.GetComponentInChildren<UIDragScrollView>();
+                if (scrollRect != null)
+                {
+                    Log.Debug("Calling CallOnPress on " + scrollRect.gameObject.name);
+                    scrollRect.CallOnPress();
+                }
+
+                scrollRect = textPlate.transform.parent.GetComponentInChildren<UIDragScrollView>();
+                if (scrollRect != null)
+                {
+                    Log.Debug("Calling CallOnPress on " + scrollRect.gameObject.name);
+                    scrollRect.CallOnPress();
+                }
+
+                var scrollView = textPlate;
                 if (scrollView != null)
                 {
-                    var scrollRect = scrollView.GetComponent<UIDragScrollView>();
+                    scrollRect = scrollView.transform.GetComponent<UIDragScrollView>();
+                    Log.Debug("Calling CallOnPress on " + scrollRect.gameObject.name);
                     scrollRect.CallOnPress();
+                }
+                else
+                {
+                    Log.Debug("Could not find UIDragScrollView");
                 }
             }
 
             yield return null;
         }
 
-        Log.Debug("PartsMonsterDetailTextPlate destroyed");
+        // Log.Debug("PartsMonsterDetailTextPlate destroyed");
     }
 }
 
